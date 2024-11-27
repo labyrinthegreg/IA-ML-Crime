66a41e27ad57c0d66f764ad88fd46d606b3faf7c
barbara michaud <barbara.michaud@outlook.fr>
2024-11-27 11:40:34 +0100

    adding frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import CrimePrediction from "./components/CrimePrediction";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <div className="App-logo">
+        <CrimePrediction />
+          </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
